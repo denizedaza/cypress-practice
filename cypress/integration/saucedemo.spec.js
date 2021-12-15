4cd2dde14c2963a8b743e85370b963733676fca4
denizedaza <dazadenize@gmail.com>
2021-12-15 15:07:44 -0500

    create tests for saucedemo page

---
+++ cypress/integration/saucedemo.spec.js
@@ -0,0 +1,22 @@
+describe("Saucedemo / swaglabs website",() => {
+  beforeEach(() => {
+    cy.visit("https://www.saucedemo.com");
+
+    const username = "standard_user";
+    const password = "secret_sauce";
+
+    cy.get('input[data-test="username"]').type(username)
+    .get('input[data-test="password"]').type(password)
+    .get('input[data-test="login-button"]').click();
+  });
+
+  it("should login correctly and land on the right page", () => {
+    cy.get(".app_logo").should('be.visible');
+  });
+
+  it("should add backpack to cart (assumes cart it empty)", () => {
+    cy.get('span.shopping_cart_badge').should('not.be.visible')
+  })
+
+
+})
